4344f669cc05b8b9715b15d28a238153520f4a2e
zai63 <72634338+zai63@users.noreply.github.com>
2020-10-10 08:32:33 +0800

    Update conf.js

--- conf.js
+++ conf.js
@@ -12,7 +12,7 @@ jsproxy_config({
     'defalut': {
       label: '第1通道',
       lines: {
-        '111.111.workers.dev': 1,
+        'winter-base-bb4d.mcquadevutyco.workers.dev': 1,
       }
     },
     
@@ -20,7 +20,7 @@ jsproxy_config({
     'defalut1': {
       label: '第2通道',
       lines: {
-        '222.222.workers.dev': 1,
+        'ancient-flower-b44f.mcquadevutyco.workers.dev': 1,
       }
     },
     
